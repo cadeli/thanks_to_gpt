11ab5e0bdf3629eb4b9dbbfaac2d25c6d9d15663
phr <philippe.reynaud@gmail.com>
2023-11-04 09:56:13 +0100

    wip

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # thanks_to_gpt
+
+a game writtten by chat gpt 3.5
+
+prompts :
+1/ write a code in javascript for a simple game using p5.js in the style of space invaders
+2/ add more advanced enemy behavior
+3/ enemy must change direction when the edge of the screen  is reached left or right 
+4/ write a "wait for space key pressed " code when the game is over
--- sketch.js
+++ sketch.js
@@ -14,13 +14,18 @@ let playerWidth = 40;
 let playerHeight = 20;
 
 // Bullet variables
-let bullets = [];
+let playerBullets = [];
+let enemyBullets = [];
 
 // Enemy variables
 let enemies = [];
 let enemyDirection = 1; // 1 for right, -1 for left
-let enemyYIncrement = 10;
+let enemyYIncrement = 20;
 let level = 1;
+
+// Game state
+let isGamePaused = false;
+let isGameOver = false;
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
@@ -33,98 +38,61 @@ function setup() {
 function draw() {
   background(0);
 
-  // Draw player
-  fill(0, 0, 255);
-  rect(playerX, playerY, playerWidth, playerHeight);
+  if (isGamePaused) {
+    fill(255);
+    textSize(24);
+    textAlign(CENTER, CENTER);
+    text("Press SPACE to start", canvasWidth / 2, canvasHeight / 2);
+  } else if (isGameOver) {
+    fill(255);
+    textSize(24);
+    textAlign(CENTER, CENTER);
+    text(`Game Over\nScore: ${levelScore}`, canvasWidth / 2, canvasHeight / 2);
+  } else {
+    // Game logic
+    movePlayer();
+    movePlayerBullets();
+    moveEnemies();
+    moveEnemyBullets();
+    checkCollisions();
 
-  // Move player
-  if (keyIsDown(LEFT_ARROW) && playerX > 0) {
-    playerX -= playerSpeed;
-  }
-  if (keyIsDown(RIGHT_ARROW) && playerX + playerWidth < canvasWidth) {
-    playerX += playerSpeed;
-  }
+    // Draw player
+    fill(0, 0, 255);
+    rect(playerX, playerY, playerWidth, playerHeight);
 
-  // Draw bullets and move them
-  for (let i = bullets.length - 1; i >= 0; i--) {
-    let bullet = bullets[i];
-    fill(255, 0, 0);
-    rect(bullet.x, bullet.y, 5, 10);
-    bullet.y -= bulletSpeed;
-
-    // Remove bullets that go off-screen
-    if (bullet.y < 0) {
-      bullets.splice(i, 1);
-    }
-  }
-
-  // Move and draw enemies
-  for (let i = 0; i < enemies.length; i++) {
-    let enemy = enemies[i];
-    fill(0, 255, 0);
-    rect(enemy.x, enemy.y, enemy.width, enemy.height);
-    enemy.x += enemyDirection * enemySpeed;
-
-    // Ensure enemies stay within the canvas boundaries
-    if (enemy.x >= ( canvasWidth - enemy.width)){
-      enemyDirection*=-1
-    }
-   // Ensure enemies stay within the canvas boundaries
-    if (enemy.x <= 0){
-      enemyDirection*=-1
-    }
-    // Enemy shoots back
-    if (random(1) < 0.001) {
-      enemyBullets.push({ x: enemy.x + enemy.width / 2, y: enemy.y });
+    // Draw bullets
+    for (let bullet of playerBullets) {
+      fill(255, 0, 0);
+      rect(bullet.x, bullet.y, 5, 10);
     }
 
-    // Check for bullet collisions with enemies
-    for (let j = bullets.length - 1; j >= 0; j--) {
-      let bullet = bullets[j];
-      if (
-        bullet.x + 5 > enemy.x &&
-        bullet.x < enemy.x + enemy.width &&
-        bullet.y < enemy.y + enemy.height
-      ) {
-        bullets.splice(j, 1);
-        enemies.splice(i, 1);
-        levelScore++;
-      }
-    }
-  }
-
-  // Move and draw enemy bullets
-  for (let i = enemyBullets.length - 1; i >= 0; i--) {
-    let enemyBullet = enemyBullets[i];
-    fill(255, 0, 0);
-    rect(enemyBullet.x, enemyBullet.y, 5, 10);
-    enemyBullet.y += bulletSpeed;
-
-    // Remove enemy bullets that go off-screen
-    if (enemyBullet.y > canvasHeight) {
-      enemyBullets.splice(i, 1);
+    for (let bullet of enemyBullets) {
+      fill(0, 255, 0);
+      rect(bullet.x, bullet.y, 5, 10);
     }
 
-    // Check for enemy bullet collisions with player
-    if (
-      enemyBullet.x + 5 > playerX &&
-      enemyBullet.x < playerX + playerWidth &&
-      enemyBullet.y + 10 > playerY
-    ) {
-      gameOver();
+    // Draw enemies
+    for (let enemy of enemies) {
+      fill(0, 255, 0);
+      rect(enemy.x, enemy.y, enemy.width, enemy.height);
     }
-  }
-
-  // Check if all enemies are destroyed to move to the next level
-  if (enemies.length === 0) {
-    level++;
-    createEnemies();
   }
 }
 
 function keyPressed() {
-  if (key === ' ' && bullets.length < 3) {
-    bullets.push({ x: playerX + playerWidth / 2, y: playerY });
+  if (isGamePaused && key === ' ') {
+    isGamePaused = false;
+  } else if (isGameOver && key === ' ') {
+    isGameOver = false;
+    isGamePaused = true; // Pause until space is pressed again to start
+    level = 1;
+    playerBullets = [];
+    enemyBullets = [];
+    createEnemies();
+  } else if (!isGamePaused && !isGameOver) {
+    if (key === ' ' && playerBullets.length < 3) {
+      playerBullets.push({ x: playerX + playerWidth / 2, y: playerY });
+    }
   }
 }
 
@@ -137,22 +105,100 @@ function createEnemies() {
         y: i * enemySpacing + 30,
         width: 30,
         height: 30,
+        direction: 1,
+        fireCooldown: Math.random() * 1200 + 10,
       });
     }
   }
 }
 
+function movePlayer() {
+  if (keyIsDown(LEFT_ARROW) && playerX > 0) {
+    playerX -= playerSpeed;
+  }
+  if (keyIsDown(RIGHT_ARROW) && playerX + playerWidth < canvasWidth) {
+    playerX += playerSpeed;
+  }
+}
+
+function movePlayerBullets() {
+  for (let i = playerBullets.length - 1; i >= 0; i--) {
+    let bullet = playerBullets[i];
+    bullet.y -= bulletSpeed;
+    if (bullet.y < 0) {
+      playerBullets.splice(i, 1);
+    }
+  }
+}
+
+function moveEnemies() {
+  for (let enemy of enemies) {
+    enemy.x += enemy.direction * enemySpeed;
+
+    if (
+      enemy.x + enemy.width >= canvasWidth ||
+      enemy.x <= 0
+    ) {
+      enemy.direction *= -1;
+      enemy.y += enemyYIncrement;
+    }
+
+    enemy.fireCooldown--;
+    if (enemy.fireCooldown <= 0) {
+      enemyBullets.push({ x: enemy.x + enemy.width / 2, y: enemy.y + enemy.height });
+      enemy.fireCooldown = Math.random() * 1200 + 10;
+    }
+  }
+}
+
+function moveEnemyBullets() {
+  for (let i = enemyBullets.length - 1; i >= 0; i--) {
+    let bullet = enemyBullets[i];
+    bullet.y += bulletSpeed;
+    if (bullet.y > canvasHeight) {
+      enemyBullets.splice(i, 1);
+    }
+  }
+}
+
+function checkCollisions() {
+  for (let i = playerBullets.length - 1; i >= 0; i--) {
+    let playerBullet = playerBullets[i];
+    for (let j = enemies.length - 1; j >= 0; j--) {
+      let enemy = enemies[j];
+      if (
+        playerBullet.x + 5 > enemy.x &&
+        playerBullet.x < enemy.x + enemy.width &&
+        playerBullet.y < enemy.y + enemy.height
+      ) {
+        playerBullets.splice(i, 1);
+        enemies.splice(j, 1);
+         levelScore++;
+      }
+    }
+  }
+
+  for (let i = enemyBullets.length - 1; i >= 0; i--) {
+    let enemyBullet = enemyBullets[i];
+    if (
+      enemyBullet.x + 5 > playerX &&
+      enemyBullet.x < playerX + playerWidth &&
+      enemyBullet.y + 10 > playerY
+    ) {
+      gameOver();
+    }
+  }
+
+  if (enemies.length === 0) {
+    level++;
+    createEnemies();
+  }
+}
+
 function gameOver() {
-  background(0);
-  fill(255);
-  textSize(32);
-  textAlign(CENTER, CENTER);
-  text(`Game Over\nScore: ${levelScore}`, canvasWidth / 2, canvasHeight / 2);
-  noLoop();
+  isGameOver = true;
+  isGamePaused = true;
 }
 
 // You can keep track of the player's score using a variable
 let levelScore = 0;
-
-// Create an array to store enemy bullets
-let enemyBullets = [];
